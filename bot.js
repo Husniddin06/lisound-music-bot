3f0511b6b6ea50ab3a999b520ffb8f4b5ef3bbfe
Husniddin06 <154774190+Husniddin06@users.noreply.github.com>
2026-03-06 15:23:55 +1000

    Add Telegram bot implementation in bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+
+bot.sendMessage(msg.chat.id,
+"🎵 LiSound Music Bot ishlayapti!");
+
+});
